7f93ef79e3ea61a5023aedbfa830ac00ac5e78f3
Victor Gaydov <victor@enise.org>
2016-05-06 18:40:52 +0300

    More Object methods and tests

--- object.go
+++ object.go
@@ -19,10 +19,30 @@ func (o *Object) Raw() map[string]interface{} {
 	return o.value
 }
 
-// TODO:
-//  Keys()
-//  Values()
-//  Value()
+func (o *Object) Keys() *Array {
+	keys := make([]interface{}, 0)
+	for k, _ := range o.value {
+		keys = append(keys, k)
+	}
+	return NewArray(o.checker.Clone(), keys)
+}
+
+func (o *Object) Values() *Array {
+	values := make([]interface{}, 0)
+	for _, v := range o.value {
+		values = append(values, v)
+	}
+	return NewArray(o.checker.Clone(), values)
+}
+
+func (o *Object) Value(key string) *Value {
+	value, ok := o.value[key]
+	if !ok {
+		o.checker.Fail("expected map containing %v, got %v", key, o.value)
+		return NewValue(o.checker.Clone(), nil)
+	}
+	return NewValue(o.checker.Clone(), value)
+}
 
 func (o *Object) Empty() *Object {
 	expected := make(map[string]interface{})
@@ -76,6 +96,50 @@ func (o *Object) NotContainsKey(key string) *Object {
 	return o
 }
 
+func (o *Object) ContainsMap(submap map[string]interface{}) *Object {
+	if !o.containsMap(submap) {
+		o.checker.Fail("expected map containing submap %v, got %v", submap, o.value)
+	}
+	return o
+}
+
+func (o *Object) NotContainsMap(submap map[string]interface{}) *Object {
+	if o.containsMap(submap) {
+		o.checker.Fail("expected map NOT containing submap %v, got %v", submap, o.value)
+	}
+	return o
+}
+
+func (o *Object) ValueEqual(k string, v interface{}) *Object {
+	if !o.containsKey(k) {
+		o.checker.Fail("expected map containing %v, got %v", k, o.value)
+		return o
+	}
+	expected, ok := canonValue(o.checker, v)
+	if !ok {
+		return o
+	}
+	actual, ok := canonValue(o.checker, o.canon[k])
+	if !ok {
+		return o
+	}
+	o.checker.Equal(expected, actual)
+	return o
+}
+
+func (o *Object) ValueNotEqual(k string, v interface{}) *Object {
+	if !o.containsKey(k) {
+		o.checker.Fail("expected map containing %v, got %v", k, o.value)
+		return o
+	}
+	expected, ok := canonValue(o.checker, v)
+	if !ok {
+		return o
+	}
+	o.checker.NotEqual(expected, o.canon[k])
+	return o
+}
+
 func (o *Object) containsKey(key string) bool {
 	for k, _ := range o.canon {
 		if k == key {
@@ -85,8 +149,18 @@ func (o *Object) containsKey(key string) bool {
 	return false
 }
 
-// TODO:
-//  ContainsMap()
-//  NotContainsMap()
-//  ValueEqual()
-//  ValueNotEqual()
+func (o *Object) containsMap(sm map[string]interface{}) bool {
+	submap, ok := canonMap(o.checker, sm)
+	if !ok {
+		return false
+	}
+	for k, v := range submap {
+		if !o.containsKey(k) {
+			return false
+		}
+		if !o.checker.Compare(v, o.canon[k]) {
+			return false
+		}
+	}
+	return true
+}
--- object_test.go
+++ object_test.go
@@ -181,6 +181,95 @@ func TestObjectContainsKey(t *testing.T) {
 	checker.Reset()
 }
 
+func TestObjectContainsMap(t *testing.T) {
+	checker := &mockChecker{}
+
+	value := NewObject(checker, map[string]interface{}{
+		"foo": 123,
+		"bar": []interface{}{"456", 789},
+		"baz": map[string]interface{}{
+			"a": "b",
+		},
+	})
+
+	submap1 := map[string]interface{}{
+		"foo": 123,
+		"bar": []interface{}{"456", 789},
+	}
+
+	value.ContainsMap(submap1)
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.NotContainsMap(submap1)
+	checker.AssertFailed(t)
+	checker.Reset()
+
+	submap2 := map[string]interface{}{
+		"bar": []interface{}{"456", 789},
+		"baz": map[string]interface{}{
+			"a": "b",
+		},
+	}
+
+	value.ContainsMap(submap2)
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.NotContainsMap(submap2)
+	checker.AssertFailed(t)
+	checker.Reset()
+
+	submap3 := map[string]interface{}{
+		"foo": 123,
+		"qux": 456,
+	}
+
+	value.ContainsMap(submap3)
+	checker.AssertFailed(t)
+	checker.Reset()
+
+	value.NotContainsMap(submap3)
+	checker.AssertSuccess(t)
+	checker.Reset()
+}
+
+func TestObjectValueEqual(t *testing.T) {
+	checker := &mockChecker{}
+
+	value := NewObject(checker, map[string]interface{}{
+		"foo": 123,
+		"bar": []interface{}{"456", 789},
+		"baz": map[string]interface{}{
+			"a": "b",
+		},
+	})
+
+	value.ValueEqual("foo", 123)
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.ValueNotEqual("foo", 123)
+	checker.AssertFailed(t)
+	checker.Reset()
+
+	value.ValueEqual("bar", []interface{}{"456", 789})
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.ValueNotEqual("bar", []interface{}{"456", 789})
+	checker.AssertFailed(t)
+	checker.Reset()
+
+	value.ValueEqual("baz", map[string]interface{}{"a": "b"})
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.ValueNotEqual("baz", map[string]interface{}{"a": "b"})
+	checker.AssertFailed(t)
+	checker.Reset()
+}
+
 func TestObjectConvertEqual(t *testing.T) {
 	type (
 		myMap map[string]interface{}
@@ -222,4 +311,100 @@ func TestObjectConvertEqual(t *testing.T) {
 	value.NotEqual(myMap{"foo": myInt(123)})
 	checker.AssertFailed(t)
 	checker.Reset()
+}
+
+func TestObjectConvertContainsMap(t *testing.T) {
+	type (
+		myArray []interface{}
+		myMap   map[string]interface{}
+		myInt   int
+	)
+
+	checker := &mockChecker{}
+
+	value := NewObject(checker, map[string]interface{}{
+		"foo": 123,
+		"bar": []interface{}{"456", 789},
+		"baz": map[string]interface{}{
+			"a": "b",
+		},
+	})
+
+	submap := myMap{
+		"foo": myInt(123),
+		"bar": myArray{"456", myInt(789)},
+	}
+
+	value.ContainsMap(submap)
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.NotContainsMap(submap)
+	checker.AssertFailed(t)
+	checker.Reset()
+}
+
+func TestObjectConvertValueEqual(t *testing.T) {
+	type (
+		myArray []interface{}
+		myMap   map[string]interface{}
+		myInt   int
+	)
+
+	checker := &mockChecker{}
+
+	value := NewObject(checker, map[string]interface{}{
+		"foo": 123,
+		"bar": []interface{}{"456", 789},
+		"baz": map[string]interface{}{
+			"a": "b",
+		},
+	})
+
+	value.ValueEqual("bar", myArray{"456", myInt(789)})
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.ValueNotEqual("bar", myArray{"456", myInt(789)})
+	checker.AssertFailed(t)
+	checker.Reset()
+
+	value.ValueEqual("baz", myMap{"a": "b"})
+	checker.AssertSuccess(t)
+	checker.Reset()
+
+	value.ValueNotEqual("baz", myMap{"a": "b"})
+	checker.AssertFailed(t)
+	checker.Reset()
+}
+
+func TestObjectGetters(t *testing.T) {
+	checker := &mockChecker{}
+
+	m := map[string]interface{}{
+		"foo": 123,
+		"bar": []interface{}{"456", 789.0},
+		"baz": map[string]interface{}{
+			"a": "b",
+		},
+	}
+
+	value := NewObject(checker, m)
+
+	keys := []interface{}{"foo", "bar", "baz"}
+
+	values := []interface{}{
+		123,
+		[]interface{}{"456", 789.0},
+		map[string]interface{}{
+			"a": "b",
+		},
+	}
+
+	assert.Equal(t, keys, value.Keys().Raw())
+	assert.Equal(t, values, value.Values().Raw())
+
+	assert.Equal(t, m["foo"], value.Value("foo").Raw())
+	assert.Equal(t, m["bar"], value.Value("bar").Raw())
+	assert.Equal(t, m["baz"], value.Value("baz").Raw())
 }
