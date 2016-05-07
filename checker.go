f143bf0deafcebf6e9c960b0f0b6d3380be9b230
Victor Gaydov <victor@enise.org>
2016-05-07 18:48:36 +0300

    Document AssertChecker and RequireChecker

--- checker.go
+++ checker.go
@@ -7,28 +7,37 @@ import (
 	"testing"
 )
 
+// AssertChecker implements Checker interface using `testify/assert' package.
+//
+// All failures are non-fatal with this checker. After first failure, all
+// futher failures within this copy of checker and its clones are ignored.
 type AssertChecker struct {
 	*assert.Assertions
 	failed bool
 }
 
+// NewAssertChecker returns a new AssertChecker object.
 func NewAssertChecker(t *testing.T) *AssertChecker {
 	return &AssertChecker{assert.New(t), false}
 }
 
+// Clone implements Checker.Clone.
 func (c *AssertChecker) Clone() Checker {
 	copy := *c
 	return &copy
 }
 
+// Compare implements Checker.Compare.
 func (_ *AssertChecker) Compare(a, b interface{}) bool {
 	return assert.ObjectsAreEqual(a, b)
 }
 
+// Failed implements Checker.Failed.
 func (c *AssertChecker) Failed() bool {
 	return c.failed
 }
 
+// Fail implements Checker.Fail.
 func (c *AssertChecker) Fail(message string, args ...interface{}) {
 	if c.failed {
 		return
@@ -37,6 +46,7 @@ func (c *AssertChecker) Fail(message string, args ...interface{}) {
 	c.failed = true
 }
 
+// Equal implements Checker.Equal.
 func (c *AssertChecker) Equal(expected, actual interface{}) {
 	if c.failed {
 		return
@@ -46,6 +56,7 @@ func (c *AssertChecker) Equal(expected, actual interface{}) {
 	}
 }
 
+// NotEqual implements Checker.NotEqual.
 func (c *AssertChecker) NotEqual(expected, actual interface{}) {
 	if c.failed {
 		return
@@ -55,34 +66,45 @@ func (c *AssertChecker) NotEqual(expected, actual interface{}) {
 	}
 }
 
+// RequireChecker implements Checker interface using `testify/require' package.
+//
+// All failures fatal with this checker. After first failure, Goexit() is called
+// and test is terminated.
 type RequireChecker struct {
 	*require.Assertions
 }
 
+// NewRequireChecker returns a new RequireChecker object.
 func NewRequireChecker(t *testing.T) *RequireChecker {
 	return &RequireChecker{require.New(t)}
 }
 
+// Clone implements Checker.Clone.
 func (c *RequireChecker) Clone() Checker {
 	return c
 }
 
+// Compare implements Checker.Compare.
 func (_ *RequireChecker) Compare(a, b interface{}) bool {
 	return assert.ObjectsAreEqual(a, b)
 }
 
+// Failed implements Checker.Failed.
 func (_ *RequireChecker) Failed() bool {
 	return false
 }
 
+// Fail implements Checker.Fail.
 func (c *RequireChecker) Fail(message string, args ...interface{}) {
 	c.Assertions.FailNow(fmt.Sprintf(message, args...))
 }
 
+// Equal implements Checker.Equal.
 func (c *RequireChecker) Equal(expected, actual interface{}) {
 	c.Assertions.Equal(expected, actual)
 }
 
+// NotEqual implements Checker.NotEqual.
 func (c *RequireChecker) NotEqual(expected, actual interface{}) {
 	c.Assertions.NotEqual(expected, actual)
 }
