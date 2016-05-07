75452be1449cdd6e00353c23cc6e8e8aea5641d7
Victor Gaydov <victor@enise.org>
2016-05-07 13:50:23 +0300

    Add example

---
+++ example/fruits.go
@@ -0,0 +1,72 @@
+package fruits
+
+import (
+	"encoding/json"
+	"net/http"
+	"path"
+)
+
+var (
+	fruits map[string]interface{} = make(map[string]interface{})
+)
+
+func FruitServer() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/fruits", handleFruitList)
+	mux.HandleFunc("/fruits/", handleFruit)
+
+	return mux
+}
+
+func handleFruitList(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		ret := make([]string, 0)
+		for k, _ := range fruits {
+			ret = append(ret, k)
+		}
+
+		b, err := json.Marshal(ret)
+		if err != nil {
+			panic(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func handleFruit(w http.ResponseWriter, r *http.Request) {
+	_, name := path.Split(r.URL.Path)
+
+	switch r.Method {
+	case "GET":
+		if data, ok := fruits[name]; ok {
+			b, err := json.Marshal(data)
+			if err != nil {
+				panic(err)
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(b)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+	case "PUT":
+		var data map[string]interface{}
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			panic(err)
+		}
+
+		fruits[name] = data
+		w.WriteHeader(http.StatusNoContent)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
