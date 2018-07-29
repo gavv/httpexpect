e27194604e2e243f3f072c0a47295df10f18fef9
Victor Gaydov <victor@enise.org>
2018-07-29 15:13:03 +0300

    Add some jsonpath tests

--- value_test.go
+++ value_test.go
@@ -2,8 +2,10 @@ package httpexpect
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -409,6 +411,137 @@ func TestValuePathError(t *testing.T) {
 		assert.True(t, bad.Raw() == nil)
 		value.chain.assertFailed(t)
 	}
+}
+
+// based on github.com/yalp/jsonpath
+func TestValuePathExpressions(t *testing.T) {
+	data := map[string]interface{}{
+		"A": []interface{}{
+			"string",
+			23.3,
+			3.0,
+			true,
+			false,
+			nil,
+		},
+		"B": "value",
+		"C": 3.14,
+		"D": map[string]interface{}{
+			"C": 3.1415,
+			"V": []interface{}{
+				"string2",
+				map[string]interface{}{
+					"C": 3.141592,
+				},
+			},
+		},
+	}
+
+	reporter := newMockReporter(t)
+
+	compare := func(expected, actual interface{}) {
+		assert.Equal(t, expected, actual)
+	}
+
+	compareUnordered := func(expected, actual interface{}) {
+		sortFn := func(obj interface{}) {
+			sort.Slice(obj, func(i, j int) bool {
+				arr := obj.([]interface{})
+				return fmt.Sprint(arr[i]) < fmt.Sprint(arr[j])
+			})
+		}
+
+		sortFn(expected)
+		sortFn(actual)
+
+		assert.Equal(t, expected, actual)
+	}
+
+	runTests := func(cmpFn func(expected, actual interface{}), tests map[string]interface{}) {
+		value := NewValue(reporter, data)
+		value.chain.assertOK(t)
+
+		for path, expected := range tests {
+			actual := value.Path(path)
+			actual.chain.assertOK(t)
+
+			cmpFn(expected, actual.Raw())
+		}
+	}
+
+	t.Run("basic", func(t *testing.T) {
+		runTests(compare, map[string]interface{}{
+			"$":          data,
+			"$.A[0]":     "string",
+			`$["A"][0]`:  "string",
+			"$.A":        []interface{}{"string", 23.3, 3.0, true, false, nil},
+			"$.A[*]":     []interface{}{"string", 23.3, 3.0, true, false, nil},
+			"$.A.*":      []interface{}{"string", 23.3, 3.0, true, false, nil},
+			"$.A.*.a":    []interface{}{},
+			"$.*.V[0]":   []interface{}{"string2"},
+			`$["B","C"]`: []interface{}{"value", 3.14},
+			"$.A[1,4,2]": []interface{}{23.3, false, 3.0},
+			`$["C","B"]`: []interface{}{3.14, "value"},
+		})
+	})
+
+	t.Run("ranges", func(t *testing.T) {
+		runTests(compare, map[string]interface{}{
+			"$.A[1:4]":  []interface{}{23.3, 3.0, true},
+			"$.A[::2]":  []interface{}{"string", 3.0, false},
+			"$.A[-2:]":  []interface{}{false, nil},
+			"$.A[:-1]":  []interface{}{"string", 23.3, 3.0, true, false},
+			"$.A[::-1]": []interface{}{nil, false, true, 3.0, 23.3, "string"},
+		})
+	})
+
+	t.Run("quotes", func(t *testing.T) {
+		runTests(compare, map[string]interface{}{
+			`$[A][0]`:    "string",
+			`$["A"][0]`:  "string",
+			`$[B,C]`:     []interface{}{"value", 3.14},
+			`$["B","C"]`: []interface{}{"value", 3.14},
+			`$[C,B]`:     []interface{}{3.14, "value"},
+			`$["C","B"]`: []interface{}{3.14, "value"},
+		})
+	})
+
+	t.Run("recursion", func(t *testing.T) {
+		runTests(compareUnordered, map[string]interface{}{
+			"$..V[1].C": []interface{}{3.141592},
+			"$..C":      []interface{}{3.1415, 3.141592},
+			`$..["C"]`:  []interface{}{3.1415, 3.141592},
+			"$.D.V..*": []interface{}{
+				"string2",
+				map[string]interface{}{
+					"C": 3.141592,
+				},
+				3.141592,
+			},
+			"$..[0]": []interface{}{"string", "string2"},
+			"$..ZZ":  []interface{}{},
+		})
+	})
+}
+
+func TestValuePathIntFloat(t *testing.T) {
+	reporter := newMockReporter(t)
+
+	data := map[string]interface{}{
+		"A": 123,
+		"B": 123.0,
+	}
+
+	value := NewValue(reporter, data)
+	value.chain.assertOK(t)
+
+	a := value.Path(`$["A"]`)
+	a.chain.assertOK(t)
+	assert.Equal(t, 123.0, a.Raw())
+
+	b := value.Path(`$["B"]`)
+	b.chain.assertOK(t)
+	assert.Equal(t, 123.0, b.Raw())
 }
 
 func TestValueSchema(t *testing.T) {
