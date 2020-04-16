7c26980b34aed071d8c8e19c05ede9aaf5adc83b
Deepak <sah.sslpu@gmail.com>
2020-04-16 21:45:46 +0530

    added context, failure & formatter types

--- expect.go
+++ expect.go
@@ -219,6 +219,14 @@ type LoggerReporter interface {
 	Reporter
 }
 
+// Formatter 
+type Formatter interface {
+	BeginAssertion(Context)
+	Success(Context)
+	Failure(Context, Failure)
+	EndAssertion(Context)
+}
+
 // DefaultRequestFactory is the default RequestFactory implementation which just
 // calls http.NewRequest.
 type DefaultRequestFactory struct{}
