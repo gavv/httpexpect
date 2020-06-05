d8cbc2ff45e72040fc89553f9280c7cc51ff0b1a
Deepak Sah <sah.sslpu@gmail.com>
2020-06-05 12:24:07 +0530

    Add Context, Failure, Formatter types (#69)

---
+++ context.go
@@ -0,0 +1,15 @@
+package httpexpect
+
+import "time"
+
+// Context contains information related to an assertion.
+// It will be inherited by nested objects through the chain struct.
+type Context struct {
+	// Name of the test
+	name      string
+	request   *Request
+	response  *Response
+	reporter  Reporter
+	rtt       *time.Duration
+	formatter Formatter
+}
