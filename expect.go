8f4e54d3755d3e1879b476e45ab5b243eb52cb75
Deepak Sah <sah.sslpu@gmail.com>
2020-06-05 12:24:07 +0530

    Add Context, Failure, Formatter types (#69)

--- expect.go
+++ expect.go
@@ -67,6 +67,7 @@ package httpexpect
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/cookiejar"
@@ -228,6 +229,14 @@ type LoggerReporter interface {
 	Reporter
 }
 
+// Formatter is used for common formatting options.
+type Formatter interface {
+	BeginAssertion(Context)
+	Success(Context)
+	Failure(Context, Failure)
+	EndAssertion(Context)
+}
+
 // DefaultRequestFactory is the default RequestFactory implementation which just
 // calls http.NewRequest.
 type DefaultRequestFactory struct{}
@@ -236,6 +245,50 @@ type DefaultRequestFactory struct{}
 func (DefaultRequestFactory) NewRequest(
 	method, urlStr string, body io.Reader) (*http.Request, error) {
 	return http.NewRequest(method, urlStr, body)
+}
+
+// DefaultFormatter is the default Formatter implementation.
+type DefaultFormatter struct{}
+
+// BeginAssertion implements Formatter.BeginAssertion.
+//
+// It is a no-op for now. Actual implementation may be
+// added later.
+func (DefaultFormatter) BeginAssertion(ctx Context) {}
+
+// Success implements Formatter.Success.
+//
+// It is a no-op for now. Actual implementation may be
+// added later.
+func (DefaultFormatter) Success(ctx Context) {}
+
+// EndAssertion implements Formatter.EndAssertion.
+//
+// It is a no-op for now. Actual implementation may be
+// added later.
+func (DefaultFormatter) EndAssertion(ctx Context) {}
+
+// Failure implements Formatter.Failure and reports failure.
+//
+// It formats the info from Context and Failure struct into
+// a string and passes it to Context.Reporter for reporting.
+func (DefaultFormatter) Failure(ctx Context, f Failure) {
+	errString := ""
+	if f.actual != nil {
+		errString = fmt.Sprintf(
+			"\nexpected:\n%s\nactual:\n%s\ndiff:\n%s",
+			dumpValue(f.expected),
+			dumpValue(f.actual),
+			diffValues(f.expected, f.actual),
+		)
+	} else {
+		errString = fmt.Sprintf(
+			"expected value not equal to:\n%s",
+			dumpValue(f.expected),
+		)
+	}
+
+	ctx.reporter.Errorf(errString)
 }
 
 // New returns a new Expect object.
---
+++ failure.go
@@ -0,0 +1,10 @@
+package httpexpect
+
+// Failure contains information about failed assertion.
+// It will be passed to Formatter when an assertion fails.
+type Failure struct {
+	// Original Error while performing an assertion
+	err      error
+	actual   *Value
+	expected *Value
+}
