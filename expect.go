ba96996fbceec7eb503b64b6fcbfc48846eb3cc2
Deepak <sah.sslpu@gmail.com>
2020-06-28 15:27:23 +0530

    added default formatter implementation

--- expect.go
+++ expect.go
@@ -66,6 +66,7 @@
 package httpexpect
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/cookiejar"
@@ -235,6 +236,50 @@ type DefaultRequestFactory struct{}
 func (DefaultRequestFactory) NewRequest(
 	method, urlStr string, body io.Reader) (*http.Request, error) {
 	return http.NewRequest(method, urlStr, body)
+}
+
+// DefaultFormatter is the default Formatter implementation.
+type DefaultFormatter struct{}
+
+// BeginAssertion implements Formatter.BeginAssertion.
+//
+// It is a no-op for now. Actual implementation may be
+// added later.
+func (DefaultFormatter) BeginAssertion(ctx Context) {}
+
+// Success implements Formatter.Success.
+//
+// It is a no-op for now. Actual implementation may be
+// added later.
+func (DefaultFormatter) Success(ctx Context) {}
+
+// EndAssertion implements Formatter.EndAssertion.
+//
+// It is a no-op for now. Actual implementation may be
+// added later.
+func (DefaultFormatter) EndAssertion(ctx Context) {}
+
+// Failure implements Formatter.Failure and reports failure.
+//
+// It formats the info from Context and Failure struct into
+// a string and passes it to Context.Reporter for reporting.
+func (DefaultFormatter) Failure(ctx Context, f Failure) {
+	errString := ""
+	if f.actual != nil {
+		errString = fmt.Sprintf(
+			"\nexpected:\n%s\nactual:\n%s\ndiff:\n%s",
+			dumpValue(f.expected),
+			dumpValue(f.actual),
+			diffValues(f.expected, f.actual),
+		)
+	} else {
+		errString = fmt.Sprintf(
+			"expected value not equal to:\n%s",
+			dumpValue(f.expected),
+		)
+	}
+
+	ctx.reporter.Errorf(errString)
 }
 
 // New returns a new Expect object.
