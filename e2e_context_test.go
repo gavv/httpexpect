9665b20862148062eae27ac6b54d63b16637a07b
gtourkas <gtourkas@gmail.com>
2021-03-23 19:21:38 +0200

    removing sleeps from tests

--- e2e_context_test.go
+++ e2e_context_test.go
@@ -11,42 +11,65 @@ import (
 	"time"
 )
 
-// the waitHandler sleeps and fails (500) for a number of calls and succeeds
-// (200) in the last one
 type waitHandler struct {
-	mux            *http.ServeMux
-	callIdx        int
-	callsDurations []time.Duration
-	callCount      int
+	mux                       *http.ServeMux
+	callCount                 int
+	retriesToFail             int
+	waitForCancelAfterRetries bool
+	retriesDone               chan struct{}
 }
 
 func (h *waitHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.mux.ServeHTTP(w, r)
 }
 
-func (h *waitHandler) Wait(w http.ResponseWriter, r *http.Request) {
+func (h *waitHandler) WaitForContextCancellation(w http.ResponseWriter, r *http.Request) {
 	h.callCount++
-	i := h.callIdx
-	if i <= len(h.callsDurations)-1 {
-		d := h.callsDurations[i]
-		time.Sleep(d)
-		w.WriteHeader(http.StatusInternalServerError)
+	// if retries-to-fail are not set then simply wait for the cancellation
+	if h.retriesToFail == 0 {
+		<-r.Context().Done()
 	} else {
+		// if retries-to-fail are set then make sure they are exhausted before
+		// waiting for cancellation
+		if h.callCount < h.retriesToFail+1 {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			h.retriesDone <- struct{}{}
+			<-r.Context().Done()
+		}
+
+	}
+}
+
+func (h *waitHandler) WaitForPerRequestTimeout(w http.ResponseWriter, r *http.Request) {
+	h.callCount++
+	// if retries-to-fail are not set or not exhausted yet, simply wait for
+	// the timeout
+	if h.retriesToFail == 0 || h.callCount < h.retriesToFail+1 {
+		<-r.Context().Done()
+	} else {
+		// otherwise succeed
 		w.WriteHeader(http.StatusOK)
 	}
 }
 
-func newWaitHandler(callsDurations []time.Duration) *waitHandler {
+func newWaitHandler(retriesToFail int) *waitHandler {
 	mux := http.NewServeMux()
 
 	handler := &waitHandler{
-		mux:            mux,
-		callsDurations: callsDurations,
+		mux:           mux,
+		retriesToFail: retriesToFail,
+		retriesDone:   make(chan struct{}),
 	}
 
-	mux.HandleFunc("/wait", handler.Wait)
+	mux.HandleFunc("/WaitForContextCancellation", handler.WaitForContextCancellation)
+	mux.HandleFunc("/WaitForPerRequestTimeout", handler.WaitForPerRequestTimeout)
 
 	return handler
+}
+
+func (h *waitHandler) waitForRetries() {
+	<-h.retriesDone
 }
 
 // the expErrorSuppressor is used as a Reporter to suppress an expected error
@@ -73,12 +96,7 @@ func (r *expErrorSuppressor) Errorf(message string, args ...interface{}) {
 }
 
 func TestGlobalContextCancel(t *testing.T) {
-
-	handler := newWaitHandler([]time.Duration{
-		time.Duration(3) * time.Second, // single call with wait 3 secs
-	})
-
-	cancelIn := time.Duration(2) * time.Second // cancel in 2 secs
+	handler := newWaitHandler(0)
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
@@ -97,27 +115,26 @@ func TestGlobalContextCancel(t *testing.T) {
 		Context:  ctx,
 	})
 
+	done := make(chan struct{})
+
 	go func() {
-		time.Sleep(cancelIn)
-		cancel()
+		e.GET("/WaitForContextCancellation").
+			Expect()
+		done <- struct{}{}
 	}()
 
-	e.GET("/wait").
-		Expect()
+	cancel()
+
+	<-done
 
 	// expected error should occur
 	assert.True(t, reporter.expErrorOccurred)
 }
 
 func TestGlobalContextWithRetries(t *testing.T) {
-
-	handler := newWaitHandler([]time.Duration{
-		time.Duration(3) * time.Second, // first call will wait 3 secs
-		time.Duration(3) * time.Second, // second call will wait 3 secs
-	}) // third call will succeed
-
-	cancelIn := time.Duration(5) * time.Second // cancel in 5 secs so that
-	// the middle call is cancelled
+	maxRetries := 3
+	retriesToFail := 2
+	handler := newWaitHandler(retriesToFail)
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
@@ -136,30 +153,28 @@ func TestGlobalContextWithRetries(t *testing.T) {
 		Context:  ctx,
 	})
 
-	// cancel in cancelIn
+	done := make(chan struct{})
+
 	go func() {
-		time.Sleep(cancelIn)
-		cancel()
+		e.GET("/WaitForContextCancellation").
+			WithMaxRetries(maxRetries).
+			Expect()
+		done <- struct{}{}
 	}()
 
-	// wait for waitFor
-	e.GET("/wait").
-		WithMaxRetries(3).
-		Expect()
+	handler.waitForRetries() // wait for the retries-set-to-fail
+	cancel()                 // cancel the rest
+
+	<-done
 
 	// expected error should occur
 	assert.True(t, reporter.expErrorOccurred)
-	// two calls; the third retry will NOT take place
-	assert.Equal(t, 2, handler.callCount)
+	// first call + retries to fail should be the call count
+	assert.Equal(t, retriesToFail+1, handler.callCount)
 }
 
 func TestPerRequestContext(t *testing.T) {
-
-	handler := newWaitHandler([]time.Duration{
-		time.Duration(3) * time.Second, // single call with wait 3 secs
-	})
-
-	cancelIn := time.Duration(2) * time.Second // cancel in 2 secs
+	handler := newWaitHandler(0)
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
@@ -177,30 +192,27 @@ func TestPerRequestContext(t *testing.T) {
 		Reporter: reporter,
 	})
 
+	done := make(chan struct{})
+
 	go func() {
-		time.Sleep(cancelIn)
-		cancel()
+		e.GET("/WaitForContextCancellation").
+			WithContext(ctx).
+			Expect()
+		done <- struct{}{}
 	}()
 
-	e.GET("/wait").
-		WithContext(ctx).
-		Expect()
+	cancel()
+
+	<-done
 
 	// expected error should occur
 	assert.True(t, reporter.expErrorOccurred)
 }
 
 func TestPerRequestContextWithRetries(t *testing.T) {
-
-	handler := newWaitHandler([]time.Duration{
-		time.Duration(3) * time.Second, // first call will wait 3 secs
-		time.Duration(3) * time.Second, // second call/first retry will
-		// wait 3 secs
-	}) // third call/second retry will
-	// succeed
-
-	cancelIn := time.Duration(5) * time.Second // cancel in 5 secs so that the
-	// middle call is cancelled
+	maxRetries := 3
+	retriesToFail := 2
+	handler := newWaitHandler(retriesToFail)
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
@@ -218,30 +230,30 @@ func TestPerRequestContextWithRetries(t *testing.T) {
 		Reporter: reporter,
 	})
 
+	done := make(chan struct{})
+
 	go func() {
-		time.Sleep(cancelIn)
-		cancel()
+		e.GET("/WaitForContextCancellation").
+			WithMaxRetries(maxRetries).
+			WithContext(ctx).
+			Expect()
+		done <- struct{}{}
 	}()
 
-	e.GET("/wait").
-		WithMaxRetries(2).
-		WithContext(ctx).
-		Expect()
+	handler.waitForRetries() // wait for the retries-set-to-fail
+	cancel()                 // cancel the rest
+
+	<-done
 
 	// expected error should occur
 	assert.True(t, reporter.expErrorOccurred)
-	// two calls: the first one and the first retry; the second retry will NOT
-	// take place
-	assert.Equal(t, 2, handler.callCount)
+	// first call + retries to fail should be the call count
+	assert.Equal(t, retriesToFail+1, handler.callCount)
 }
 
 func TestPerRequestWithTimeout(t *testing.T) {
 
-	handler := newWaitHandler([]time.Duration{
-		time.Duration(3) * time.Second, // single call will wait 3 secs
-	})
-
-	timeoutAt := time.Duration(2) * time.Second // time out at 2 secs
+	handler := newWaitHandler(0)
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
@@ -256,50 +268,35 @@ func TestPerRequestWithTimeout(t *testing.T) {
 		Reporter: reporter,
 	})
 
-	e.GET("/wait").
-		WithTimeout(timeoutAt).
+	e.GET("/WaitForPerRequestTimeout").
+		WithTimeout(time.Duration(1) * time.Second).
 		Expect()
 
 	// expected error should occur
 	assert.True(t, reporter.expErrorOccurred)
-	// one call; timed-out
-	assert.Equal(t, 1, handler.callCount)
 }
 
 func TestPerRequestWithTimeoutAndWithRetries(t *testing.T) {
 
-	handler := newWaitHandler([]time.Duration{
-		time.Duration(3) * time.Second, // first call will wait 3 secs
-		time.Duration(3) * time.Second, // second call/first retry will
-		// wait 3 secs
-	}) // third call/second retry will
-	// not wait
-
-	timeoutAt := time.Duration(2) * time.Second // timeout at 2 secs; both first
-	// and second calls will time
-	// out
+	maxRetries := 3
+	retriesToFail := 2
+	handler := newWaitHandler(retriesToFail)
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
-	// config with context deadline expected error
-	reporter := newExpErrorSuppressor(t,
-		func(message string, args ...interface{}) bool {
-			return strings.HasSuffix(message, "context deadline exceeded")
-		})
+	// this call will terminate with success
 	e := WithConfig(Config{
 		BaseURL:  server.URL,
-		Reporter: reporter,
+		Reporter: NewAssertReporter(t),
 	})
 
-	e.GET("/wait").
-		WithTimeout(timeoutAt).
-		WithMaxRetries(2).
+	e.GET("/WaitForPerRequestTimeout").
+		WithTimeout(time.Duration(1) * time.Second).
+		WithMaxRetries(maxRetries).
 		Expect().
-		Status(200)
+		Status(http.StatusOK)
 
-	// expected error should occur
-	assert.True(t, reporter.expErrorOccurred)
-	// three calls; the two timed-out and the one that normally returned
-	assert.Equal(t, 3, handler.callCount)
+	// first call + retries to fail should be the call count
+	assert.Equal(t, retriesToFail+1, handler.callCount)
 }
