6cf10adb78e29bafca0d72e56a34e9069efe5a41
Victor Gaydov <victor@enise.org>
2022-11-22 19:03:15 +0400

    Improve formatter tests

--- formatter_test.go
+++ formatter_test.go
@@ -1,12 +1,75 @@
 package httpexpect
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFormatDiffErrors(t *testing.T) {
+type typedNil int
+
+func (*typedNil) String() string {
+	return ""
+}
+
+func TestFormatValues(t *testing.T) {
+	checkAll := func(t *testing.T, fn func(interface{}) string) {
+		var tnil *typedNil
+		var tnilPtr fmt.Stringer = tnil
+
+		assert.Nil(t, tnilPtr)
+		assert.NotEqual(t, nil, tnilPtr)
+
+		check := func(s string) {
+			t.Logf("\n%s", s)
+			assert.NotEmpty(t, s)
+		}
+
+		check(fn(nil))
+		check(fn(tnil))
+		check(fn(tnilPtr))
+		check(fn(123))
+		check(fn("hello"))
+		check(fn(time.Second))
+		check(fn(time.Unix(0, 0)))
+		check(fn([]interface{}{1, 2}))
+		check(fn(map[string]string{"a": "b"}))
+		check(fn(make(chan int)))
+		check(fn(AssertionRange{1, 2}))
+		check(fn(&AssertionRange{1, 2}))
+		check(fn(AssertionRange{"a", "b"}))
+		check(fn(AssertionList([]interface{}{1, 2})))
+	}
+
+	t.Run("formatTypes", func(t *testing.T) {
+		checkAll(t, formatTyped)
+	})
+
+	t.Run("formatValue", func(t *testing.T) {
+		checkAll(t, formatValue)
+	})
+
+	t.Run("formatString", func(t *testing.T) {
+		checkAll(t, formatString)
+	})
+
+	t.Run("formatRange", func(t *testing.T) {
+		checkAll(t, func(v interface{}) string {
+			return strings.Join(formatRange(v), "")
+		})
+	})
+
+	t.Run("formatList", func(t *testing.T) {
+		checkAll(t, func(v interface{}) string {
+			return strings.Join(formatList(v), "")
+		})
+	})
+}
+
+func TestFormatDiff(t *testing.T) {
 	checkOK := func(a, b interface{}) {
 		s, ok := formatDiff(a, b)
 		assert.True(t, ok)
