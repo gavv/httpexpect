e74190344b29067076e0cea7230feb87193b77bd
Mor Kaire <kairemor@gmail.com>
2023-01-10 10:51:33 +0000

    format

--- value.go
+++ value.go
@@ -9,8 +9,8 @@ import (
 // (Go representation of arbitrary JSON value) and cast it to
 // concrete type.
 type Value struct {
-	chain  *chain
-	value  interface{}
+	chain *chain
+	value interface{}
 }
 
 // NewValue returns a new Value instance.
