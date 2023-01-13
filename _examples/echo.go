fde24042edc774fe5b70f7d73f7e4cb31bbf8fd0
Victor Gaydov <victor@enise.org>
2023-01-13 17:50:18 +0400

    Bump echo in examples to v4

--- _examples/echo.go
+++ _examples/echo.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 // EchoHandler creates http.Handler using echo framework.
