98a4ab1f91fae175608c6790700ac2ab14c31608
Fahim Bagar <fahim.bagar@gmail.com>
2023-01-27 00:52:37 +0700

    Unit test in printer.go

--- printer_test.go
+++ printer_test.go
@@ -2,9 +2,12 @@ package httpexpect
 
 import (
 	"bytes"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPrinter_Compact(t *testing.T) {
@@ -41,4 +44,40 @@ func TestPrinter_Debug(t *testing.T) {
 	printer.Response(&http.Response{Body: ioutil.NopCloser(body2)}, 0)
 	printer.Response(&http.Response{}, 0)
 	printer.Response(nil, 0)
+}
+
+type panicReader struct{}
+
+func (p panicReader) Read(_ []byte) (n int, err error) {
+	return 0, errors.New("error")
+}
+
+func (p panicReader) Close() error {
+	return errors.New("error")
+}
+
+func TestPrinter_Panics(t *testing.T) {
+	t.Run("panic_curl_printer", func(t *testing.T) {
+		curl := NewCurlPrinter(t)
+
+		assert.Panics(t, func() {
+			curl.Request(&http.Request{})
+		})
+	})
+
+	t.Run("panic_debug_printer", func(t *testing.T) {
+		curl := NewDebugPrinter(t, true)
+
+		assert.Panics(t, func() {
+			curl.Request(&http.Request{
+				Body: &panicReader{},
+			})
+		})
+
+		assert.Panics(t, func() {
+			curl.Response(&http.Response{
+				Body: &panicReader{},
+			}, 0)
+		})
+	})
 }
