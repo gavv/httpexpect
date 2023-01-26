3ca23bed4e90a3c6f19a18b02efb3d30332e922a
Edwin Wijaya <edwin.wijaya@tokopedia.com>
2023-01-26 21:16:49 +0700

    update custom formatter examples

--- _examples/custom_formatter_test.go
+++ _examples/custom_formatter_test.go
@@ -19,6 +19,12 @@ Test Failed!
 	reference value: {{ .Reference }}
 `
 
+var successTemplate = `
+Test Success!
+	test name: {{ .TestName | underscore }}
+	request name: {{ .RequestName }}
+`
+
 var templateFuncs = template.FuncMap{
 	"underscore": func(s string) string {
 		var sb strings.Builder
@@ -30,15 +36,18 @@ var templateFuncs = template.FuncMap{
 	},
 }
 
-func TestDefaultFormatter(t *testing.T) {
-	handler := CustomFormatterHandler()
+// TestFailureTemplate tests with custom `FailureTemplate`
+func TestFailureTemplate(t *testing.T) {
+	t.Skip() // remove this line to see test failure
+
+	handler := FruitsHandler()
 
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
-	// customize formatting template
+	// customize error message with `FailureTemplate`
 	e := httpexpect.WithConfig(httpexpect.Config{
-		TestName: "Check Fruits Name",
+		TestName: "Check Fruits List",
 		BaseURL:  server.URL,
 		Reporter: httpexpect.NewAssertReporter(t),
 		Formatter: &httpexpect.DefaultFormatter{
@@ -47,11 +56,55 @@ func TestDefaultFormatter(t *testing.T) {
 		},
 	})
 
+	orange := map[string]interface{}{
+		"weight": 100,
+	}
+
+	e.PUT("/fruits/orange").WithJSON(orange).
+		Expect().
+		Status(http.StatusNoContent).NoContent()
+
 	fruits := e.GET("/fruits").WithName("Get Fruits").
 		Expect().
 		Status(http.StatusOK).JSON().Array()
-	fruits.Every(func(index int, value *httpexpect.Value) {
-		value.String().NotEmpty()
-	})
+
 	fruits.ContainsAny("melon")
+}
+
+// TestSuccessTemplate tests with custom `SuccessTemplate`
+func TestSuccessTemplate(t *testing.T) {
+
+	handler := FruitsHandler()
+
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// Customize success message with `SuccessTemplate`.
+	// To make it prints on success we need to set `AssertionHandler`
+	// with `t` as the `Logger`
+	e := httpexpect.WithConfig(httpexpect.Config{
+		TestName: "Check Fruits List",
+		BaseURL:  server.URL,
+		AssertionHandler: &httpexpect.DefaultAssertionHandler{
+			Formatter: &httpexpect.DefaultFormatter{
+				SuccessTemplate: successTemplate,
+				TemplateFuncs:   templateFuncs,
+			},
+			Logger: t,
+		},
+	})
+
+	orange := map[string]interface{}{
+		"weight": 100,
+	}
+
+	e.PUT("/fruits/orange").WithJSON(orange).
+		Expect().
+		Status(http.StatusNoContent).NoContent()
+
+	fruits := e.GET("/fruits").WithName("Get Fruits").
+		Expect().
+		Status(http.StatusOK).JSON().Array()
+
+	fruits.ContainsAny("orange")
 }
