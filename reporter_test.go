368aa573e7f03fadb075c9a93b884568e9f2cdfe
Nayeem Hasan Anik <nayeemhasan.nh01@gmail.com>
2023-01-28 14:50:49 +0600

    Add unit tests for reporters (#247)

--- reporter_test.go
+++ reporter_test.go
@@ -24,3 +24,40 @@ func TestReporter_FatalReporter(t *testing.T) {
 		assert.True(t, mockBackend.fatalfInvoked)
 	})
 }
+
+type mockAssertT struct {
+	failInvoked bool
+}
+
+func (m *mockAssertT) Errorf(format string, args ...interface{}) {
+	m.failInvoked = true
+}
+
+func TestReporter_AssertReporter(t *testing.T) {
+	mockBackend := &mockAssertT{}
+	reporter := NewAssertReporter(mockBackend)
+
+	t.Run("Errorf", func(t *testing.T) {
+		reporter.Errorf("Test failed with backend: %v", mockBackend)
+		assert.True(t, mockBackend.failInvoked)
+	})
+}
+
+type mockRequireT struct {
+	testing.T
+	failNowInvoked bool
+}
+
+func (m *mockRequireT) FailNow() {
+	m.failNowInvoked = true
+}
+
+func TestReporter_RequireReporter(t *testing.T) {
+	mockBackend := &mockRequireT{}
+	reporter := NewRequireReporter(mockBackend)
+
+	t.Run("Errorf", func(t *testing.T) {
+		reporter.Errorf("Test failed with backend: %v", mockBackend)
+		assert.True(t, mockBackend.failNowInvoked)
+	})
+}
