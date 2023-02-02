9c6bedec4d07b816e4c96cf9c9662ed6e3452492
ROHITH RAJU <rohithraju488@gmail.com>
2023-02-02 17:08:18 +0530

    deadlock
    Signed-off-by: ROHITH RAJU <rohithraju488@gmail.com>

--- mocks_test.go
+++ mocks_test.go
@@ -183,6 +183,7 @@ func (l *mockLogger) Logf(message string, args ...interface{}) {
 type mockReporter struct {
 	testing  *testing.T
 	reported bool
+	reportCb func()
 }
 
 func newMockReporter(t *testing.T) *mockReporter {
@@ -190,6 +191,9 @@ func newMockReporter(t *testing.T) *mockReporter {
 }
 
 func (r *mockReporter) Errorf(message string, args ...interface{}) {
+	if r.reportCb != nil {
+		r.reportCb()
+	}
 	r.testing.Logf("Fail: "+message, args...)
 	r.reported = true
 }
