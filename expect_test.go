6c2e78d3986a57da639708d2fe25736fd6f2f5a3
neverbeenthisweeb <rf.ramadhana@gmail.com>
2023-04-03 17:47:06 +0700

    Simplify test

--- expect_test.go
+++ expect_test.go
@@ -350,12 +350,9 @@ func TestExpect_ErrorPropagation(t *testing.T) {
 		}
 
 		arr := NewArray(reporter, []interface{}{"foo"})
-		op1 := arr.IsEmpty()
-		op2 := op1.IsEmpty() // Does not report failure
+		arr.IsEmpty().IsEmpty() // The second .IsEmpty() call does not report failure
 
 		arr.chain.assertFlags(t, flagFailed)
-		op1.chain.assertFlags(t, flagFailed)
-		op2.chain.assertFlags(t, flagFailed)
 		assert.Equal(t, true, reporter.reported)
 		assert.Equal(t, 1, ctr)
 	})
@@ -368,14 +365,12 @@ func TestExpect_ErrorPropagation(t *testing.T) {
 		}
 
 		arr := NewArray(reporter, []interface{}{"foo"})
-		arrOp := arr.IsEmpty()
+		arr.IsEmpty()
 		val := arr.Value(0)
-		valOp := val.IsEqual("bar") // Does not report failure
+		val.IsEqual("bar") // Does not report failure
 
 		arr.chain.assertFlags(t, (flagFailed | flagFailedChildren))
-		arrOp.chain.assertFlags(t, (flagFailed | flagFailedChildren))
 		val.chain.assertFlags(t, flagFailed)
-		valOp.chain.assertFlags(t, flagFailed)
 		assert.Equal(t, true, reporter.reported)
 		assert.Equal(t, 1, ctr)
 	})
