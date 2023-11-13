aebee65388552465d50f581d13ce7dfa0976d744
Jozef Hoschek <xhoschek@fi.muni.cz>
2023-11-13 13:45:29 +0100

    new certificates and fixed panic

--- _examples/tls.go
+++ _examples/tls.go
@@ -11,74 +11,44 @@ import (
 	"strconv"
 )
 
-const rootPEM = `
+func NewRootCertPool() *x509.CertPool {
+	const rootPEM = `
 -----BEGIN CERTIFICATE-----
-MIIBVDCB+6ADAgECAgEBMAoGCCqGSM49BAMCMBIxEDAOBgNVBAoTB1Rlc3QgQ0Ew
-HhcNMjMxMTEyMjI1MDUxWhcNMjQwNTEwMjI1MDUyWjASMRAwDgYDVQQKEwdUZXN0
-IENBMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAExCfkUYqpDK4p8kqoiv8N3NJN
-TLRPXO34/bHMK1LlLgKZl/pVNoRDkBezOuA1JY7P84yIbHQURrdSk1fxSPuSYaNC
-MEAwDgYDVR0PAQH/BAQDAgIEMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFGzL
-/3opNd1chvJe3FfNKD0/r500MAoGCCqGSM49BAMCA0gAMEUCIQCQlXyc7ZOG/Pzm
-1EXeRIk+kfhTSjm2N9VU2kfK9sXZygIgSSv3lfL+sIr/HsWU0JXgadKgTQXpLdv3
-tpQZpaV/Nxc=
+MIIBUzCB+6ADAgECAgEBMAoGCCqGSM49BAMCMBIxEDAOBgNVBAoTB1Rlc3QgQ0Ew
+HhcNMjMxMTEzMTIyNTEzWhcNMjQwNTExMTIyNTEzWjASMRAwDgYDVQQKEwdUZXN0
+IENBMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEHBm1CiEs4CKw0ynUlzaTz9Pi
+ROnBwosfX3xYIEz5l1rN119FEJLWQFx8xBASkpZDz+Eehw9QdPaqwapDKGVgbaNC
+MEAwDgYDVR0PAQH/BAQDAgIEMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFExX
+luCP8Bp73F1L7UuFCM/NFgPkMAoGCCqGSM49BAMCA0cAMEQCIFLFQRgIUbjzA0c1
+Pennq6gP/WiJpppZPQq5IYR4V7BfAiBVoGh+32UOJ13YYO8HsL/6P7KIwZKXkJpJ
+LoibTriVMg==
 -----END CERTIFICATE-----
 `
-
-func ExampleTlsServer() *httptest.Server {
-	certPem := []byte(`-----BEGIN CERTIFICATE-----
-MIIC5TCCAc2gAwIBAgIBATANBgkqhkiG9w0BAQsFADASMRAwDgYDVQQKEwdBY21l
-IENvMB4XDTIzMTExMjIyNTgwNloXDTI0MDUxMDIyNTgwNlowEjEQMA4GA1UEChMH
-QWNtZSBDbzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKIVbmvhGdNJ
-ni4e3Bwxolg7M6caoud0IJHQvzyK/XwuPk3Gwp3+fjtC3SuPf8cxPhur9hCskFuK
-ngQ28T6cANUCYTjPsYY8PDOPLTTrkuuBFDdO/IxDZvZuBW3ZO3QqUAECSt8stsh8
-wheuoBlzR+hKwJJK3pmkEaNKrLmZXopqXuecW1SSteqhzf763zkVi8ZLwJziKUuD
-FjC4R5ChEq0pDzBKQdBWIv8Jptv7kIT1fN39Yiim4pZ2KJ9atjMx1ukrrBd0nKB/
-7C5nu/zlbNsot6zc8Su8Z5WCQqXoCCrPIaYnlh/+Lq9VJhYD0Pjo60xKWzzbIi+G
-EjIDn9XTUg8CAwEAAaNGMEQwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQMMAoGCCsG
-AQUFBwMBMAwGA1UdEwEB/wQCMAAwDwYDVR0RBAgwBocEfwAAATANBgkqhkiG9w0B
-AQsFAAOCAQEAcbIeCvXJpjH8QjPE7agC5rstxuRvbYHtSn7hKixojwPAFKXIQqRZ
-2l/GhJUTmymtc+vaAVSLpRAps+TuB/TXetdJqT8aadk6GFgd8FLMARCpMy5xrSSv
-qVO99viHSIpaR/T1HDKCSAz6llEId1yi+XCYR0fOSg5tXqwF8py6N19Lcp1Zy1f6
-0Ms5Pa7X2crF5f1bkoJiFM3XFTWOUy8MXuZuQoeOtGdLAePVWvQWWwZsO+2NuQkF
-7dcimZJ8gpp1QcGEFx8+RIgGkbtp1CMEx8BEf5kkfMbje1/OdZvwsfZOTI1RaUWA
-ABxPrh6fMq68uLduLGXiIoiV/yP3Lm/Xdw==
------END CERTIFICATE-----
-
-`)
-	keyPem := []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAohVua+EZ00meLh7cHDGiWDszpxqi53QgkdC/PIr9fC4+TcbC
-nf5+O0LdK49/xzE+G6v2EKyQW4qeBDbxPpwA1QJhOM+xhjw8M48tNOuS64EUN078
-jENm9m4Fbdk7dCpQAQJK3yy2yHzCF66gGXNH6ErAkkremaQRo0qsuZleimpe55xb
-VJK16qHN/vrfORWLxkvAnOIpS4MWMLhHkKESrSkPMEpB0FYi/wmm2/uQhPV83f1i
-KKbilnYon1q2MzHW6SusF3ScoH/sLme7/OVs2yi3rNzxK7xnlYJCpegIKs8hpieW
-H/4ur1UmFgPQ+OjrTEpbPNsiL4YSMgOf1dNSDwIDAQABAoIBAAib7M6MGUwQt/cp
-KnXQ6ReYpWi10HtMvsIf/Vhg5Y/oAOUurn2n29qX9ZlvuNDCu9LKcnp2QACsvzHo
-HS4/KQgnZTSYS4yevG/cpgEOljIuG/3IEz/8AIcMVvt7s127NZ6oGYP7IwZJIiIR
-420Wo3YiKlJa6bHtdgZfXAdLryrY3+PxkGwXCoETUlvHks5QBFXYmLLjfKwqGreX
-RPFkDq0OBlYDbIrfddR8iM3YZao+2dDSCFKBU40RKzY8lAU7cz/IoS4ooQ+1ouuj
-77bkQaE9C4a9DA+U4nXUAkqiWk1v0hP5YgydNPAxtfkWKJoGS/WS41ZcLWUgQ5O+
-zdP7G7kCgYEA05WmK3YTG+IcW9t3fOPLOEGvKdE8l/iA7EyXcwunYzE2v47ebciF
-vXmEye2qRlvfogD3o6YrbjDQPYhBQmmr1WnGWNgpLC+shy3oOrb1e4OBV7EHoXhW
-MeWfUoAmWGgBddU/ZfPyIoarsJMdvNu3fVGoncwuDGGyX6pumb/gcw0CgYEAxBun
-eFVhxgC9FdYBPr9HCvEqMOdulyjin28M3GnWmdrDqBlP/QuQgqXsnAn0XlZZb4Qf
-CDudDyyZncSG8AODeFMPEB5vdWqroq7zTsMufRwI+4qJSJgxVHLitWnh+egnWsmq
-fBFVG5K8RgSeB77Yl4oyejRVcOZ9Fi2vh8GZwosCgYBa5dCUnU5KTVJ3mAp2SfqV
-OYrCAVTxyN3CJolt8FTCBXOKyhr+uQXTx6/nfEYJohCqLZY15P6FgU0FElNO78zV
-i3Kd2oedpwGMtYkuKEm//VgEz1YC5YrKNubCb7GJi20NLUbmSu38LTT3T8yXxSDI
-Itu4pu4lfZc/CB4pyUfoxQKBgQC5nGcEuONisd5FpZjmF8qY66uAP/vnLDZaqpPk
-pnQMiQc4ukR//4sWbQ8mnTFifJ4Hs2hftXSxIQiAT7tbvieYIh0Wp4fc/UpYHviA
-qrH8jiVeV0Aaqpm+EULMa9wLWZSuFEO9S/Zes6JpLwOX1yVPQOkHyzK3OiBYdoM1
-naL3gwKBgFIXQ8mgtpsA9AnzrfdigKP4C78HxvEq2iLR6HLGurmtdb88rGX9qZnT
-zcXCOJ4ZS+ha08YPor/NOoI4olz8GTa0qTORcxGKsAe6F0tln0p7AeKd37jaebdU
-j0IkvdefQeTVjMZKhO1px6FMl8Jg2kWaPchU5JQb9LnMNLtv/FlG
------END RSA PRIVATE KEY-----
-`)
-
 	roots := x509.NewCertPool()
 	ok := roots.AppendCertsFromPEM([]byte(rootPEM))
 	if !ok {
 		panic("failed to parse root certificate")
 	}
+	return roots
+}
+
+func ExampleTlsServer() *httptest.Server {
+	certPem := []byte(`-----BEGIN CERTIFICATE-----
+MIIBbDCCARKgAwIBAgIBAjAKBggqhkjOPQQDAjASMRAwDgYDVQQKEwdUZXN0IENB
+MB4XDTIzMTExMzEyMjUxN1oXDTI0MDUxMTEyMjUxN1owEjEQMA4GA1UEChMHQWNt
+ZSBDbzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABEvlkPnSh5jYMD4MSkjJH7HW
+iDR/UnqIJrI3nV0FTotWly0z3nMy0FCM1VxyGJc8HcKi2KPIaQmVF2sYCLwu8xuj
+WTBXMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcDATAfBgNVHSME
+GDAWgBRMV5bgj/Aae9xdS+1LhQjPzRYD5DAPBgNVHREECDAGhwR/AAABMAoGCCqG
+SM49BAMCA0gAMEUCIQDHQVvWrOvagkYT9/qeSZ7xUwTTWiRfvWmlCgLf5NXu7AIg
+ea/Q6OcG41k25PXVn3VRLRBEfSFIsuJzTyTNXCHx8vY=
+-----END CERTIFICATE-----`)
+
+	keyPem := []byte(`-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIHHIE/n9wJI/dm1vnwhd8Jm/Wi04R+m8wYfUnkCFu4QnoAoGCCqGSM49
+AwEHoUQDQgAES+WQ+dKHmNgwPgxKSMkfsdaINH9SeogmsjedXQVOi1aXLTPeczLQ
+UIzVXHIYlzwdwqLYo8hpCZUXaxgIvC7zGw==
+-----END EC PRIVATE KEY-----`)
 
 	cert, err := tls.X509KeyPair(certPem, keyPem)
 	if err != nil {
@@ -97,15 +67,16 @@ func TlsHandler() http.Handler {
 
 	mux.HandleFunc("/tls/", func(writer http.ResponseWriter, request *http.Request) {
 		_, name := path.Split(request.URL.Path)
-		var data int
-		if err := json.NewDecoder(request.Body).Decode(&data); err != nil {
-			panic(err)
-		}
 
 		switch request.Method {
 		case "PUT":
+			var data int
+			if err := json.NewDecoder(request.Body).Decode(&data); err != nil {
+				panic(err)
+			}
 			items[name] += data
 			writer.WriteHeader(http.StatusNoContent)
+
 		case "DELETE":
 			if _, ok := items[name]; ok {
 				delete(items, name)
@@ -113,12 +84,17 @@ func TlsHandler() http.Handler {
 			} else {
 				writer.WriteHeader(http.StatusNotFound)
 			}
+
 		case "GET":
-			if _, ok := items[name]; ok {
-				writer.Write([]byte(strconv.Itoa(items[name])))
+			if amount, ok := items[name]; ok {
+				_, err := writer.Write([]byte(strconv.Itoa(amount)))
+				if err != nil {
+					writer.WriteHeader(http.StatusServiceUnavailable)
+				}
 			} else {
 				writer.WriteHeader(http.StatusNotFound)
 			}
+
 		default:
 			writer.WriteHeader(http.StatusBadRequest)
 		}
